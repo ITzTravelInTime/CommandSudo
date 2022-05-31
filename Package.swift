7dca8364d320e5aa868a1285233217420f06cc5f
ITzTravelInTime <pietrocaruso97@yahoo.it>
2022-06-01 00:23:28 +0200

    Updated the Command dependancy.

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/ITzTravelInTime/Command", from: "2.1.2"),
+        .package(url: "https://github.com/ITzTravelInTime/Command", from: "2.1.3"),
         .package(url: "https://github.com/ITzTravelInTime/TINURecovery", from: "5.0.0"),
         .package(url: "https://github.com/ITzTravelInTime/TINUNotifications", .branch("main")),
         .package(url: "https://github.com/ITzTravelInTime/SwiftLoggedPrint", from: "3.1.2")
